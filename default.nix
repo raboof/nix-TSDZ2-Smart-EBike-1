829960e3d8a035c40aa58d907f8086a6edfcad16
Arnout Engelen <arnout@bzzt.net>
2021-08-10 11:14:11 +0000

    Avoid relocation error by not including debugging symbols

--- default.nix
+++ default.nix
@@ -8,13 +8,17 @@ stdenv.mkDerivation rec {
   pname = "TSDZ2-Smart-EBike-1";
   version = "20.1C.1-VLCD5-VLCD6-XH18";
 
-  #src = /home/aengelen/dev/TSDZ2-Smart-EBike-1;
   src = fetchFromGitHub {
     owner = "emmebrusa";
     repo = "TSDZ2-Smart-EBike-1";
     rev = "v${version}";
     hash = "sha256:1ixp9f8hj1g25kmw3q2ivb8jbzfz2i11x0al8hnrgyvvlri1sv9m";
   };
+
+  patches = [
+    # https://sourceforge.net/p/sdcc/bugs/2906/
+    ./avoid-creating-debugging-symbols.patch
+  ];
 
   nativeBuildInputs = [
     (sdcc.overrideAttrs(old: {
